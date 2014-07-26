19bcc223f85e36016d7872d0b8a488ddf7ef71dd
Adrian <modernamun@gmail.com>
2014-07-26 12:22:54 -0500

    Working on displaying choices sent via JSON

--- public/js/survey_take.js
+++ public/js/survey_take.js
@@ -5,29 +5,48 @@ $(document).ready(function() {
 
     var choice_id = $("input[type='radio'][name='choice_id']:checked").val();
 
+    if (choice_id === undefined) {
+      // show an error message
+      alert("Please select one of the choices.");
+    }
+    else {
 
-    $.ajax({
-      url: "/survey/" + $("#survey_id").val() + "/take",
-      type: 'POST',
-      dataType: "json",
-      data: { 
-          survey_response_id: survey_response_id,
-          choice_id: choice_id,
-          completed_survey_id: completed_survey_id
+      $.ajax({
+        url: "/survey/" + $("#survey_id").val() + "/take",
+        type: 'POST',
+        dataType: "json",
+        data: { 
+            survey_response_id: survey_response_id,
+            choice_id: choice_id,
+            completed_survey_id: completed_survey_id
+          },
+        success: function(data){
+          console.log(data)
+           $("#question_container").html("");
+
+           if ( data.title != undefined) {
+             $("#question_container").append(data.title);
+             $("#question_container").append('<input type="hidden" id="question_id" name="question_id" value="' + data.question_next_id + '">');
+
+              for data.choices.each {
+                
+              }
+           }
+           else {
+            $("#question_container").append("<h1>Thanks for taking our survey!</h1>");
+           }
+
+           // $("#question_submit").css("display", "none")
+           // $(".survey_content").append("<h4>" + data.title + "</h4>")
+           // $(".survey_content").append("<p>" + data.option + "</p>")
+          // do something like change the page all question and choices go in question_container div
         },
-      success: function(data){
-        console.log(data)
-         $("#question_container").remove();
-         $("#question_submit").css("display", "none")
-         $(".survey_content").append("<h4>" + data.title + "</h4>")
-         $(".survey_content").append("<p>" + data.option + "</p>")
-        // do something like change the page all question and choices go in question_container div
-      },
-      error: function(returnValue){
+        error: function(returnValue){
 
-        alert(returnValue.status + ": Something bad happened.");
-      }
-    });
+          alert(returnValue.status + ": Something bad happened.");
+        }
+      });
+    }
 
   });
 });
